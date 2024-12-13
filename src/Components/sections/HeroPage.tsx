ea69964be5f132c57e2a3e635b59374dbbfd007b
Ayush5071 <ayusht5071@gmail.com>
2024-12-13 15:28:07 +0530

    Update: logo added

--- src/Components/sections/HeroPage.tsx
+++ src/Components/sections/HeroPage.tsx
@@ -5,6 +5,7 @@ import { WavyBackground } from "../ui/HomePage";
 function HeroPage() {
   return (
     <WavyBackground className="max-w-4xl mx-auto pb-40">
+      
       <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
         GREEN CLUUB MNNIT
       </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,34 @@ import { Mission } from "@/Components/sections/Mission";
 import { Testimonials } from "@/Components/sections/Testimonials";
 import { FloatingNav } from "@/Components/ui/FloatingNav";
 import { navItems } from "@/lib/data/data";
+import Image from "next/image";
+
 export default function Home() {
-    return (
+  return (
     <div className="relative w-full h-full overflow-hidden bg-zinc-950">
-      <FloatingNav navItems={navItems}/>
-      <HeroPage/>
-      <Context/>
-      <About/>
-      <Mission/>
-      <Scroller/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
+      {/* Check the logo */}
+      <div className="absolute z-40 translate-y-2 sm:translate-y-0 w-[20vw] sm:w-[20vw] md:w-[14vw] lg:w-[8vw] h-auto max-w-full">
+  <Image
+    src="/Images/logo.webp"
+    alt="Logo"
+    width={1000}  
+    height={1000} 
+    className="w-full h-auto"   
+  />
+</div>
+
+
+
+      <FloatingNav navItems={navItems} />
+      <HeroPage />
+      <Context />
+      <About />
+      <Mission />
+      <Scroller />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
+
